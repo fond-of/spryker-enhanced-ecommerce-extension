9667c416f1dc2602ef193673480cb3577f5d37c5
Pascal Fischer <p-fischer@gmx.net>
2020-12-18 11:01:10 +0100

    remove request from method declaration

--- src/FondOfSpryker/Yves/EnhancedEcommerceExtension/Dependency/EnhancedEcommerceDataLayerExpanderPluginInterface.php
+++ src/FondOfSpryker/Yves/EnhancedEcommerceExtension/Dependency/EnhancedEcommerceDataLayerExpanderPluginInterface.php
@@ -8,20 +8,18 @@ interface EnhancedEcommerceDataLayerExpanderPluginInterface
 {
     /**
      * @param string $pageType
-     * @param Request $request
      * @param array $twigVariableBag
      *
      * @return bool
      */
-    public function isApplicable(string $pageType, Request $request, array $twigVariableBag = []): bool;
+    public function isApplicable(string $pageType, array $twigVariableBag = []): bool;
 
     /**
      * @param string $page
-     * @param Request $request
      * @param array $twigVariableBag
      * @param array $dataLayer
      *
      * @return array
      */
-    public function expand(string $page, Request $request, array $twigVariableBag, array $dataLayer): array;
+    public function expand(string $page, array $twigVariableBag, array $dataLayer): array;
 }
